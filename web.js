174122977bb873cafcbb1cb2ab3eca1e44841653
Martin Molina <jmmolinaes@gmail.com>
2013-07-10 21:59:30 -0500

    web.js reads index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+	var buf= new Buffer(data);
+	response.send( buf.toString('utf8', 0, len));  
+	//console.log(data);
+});
+  
 });
 
 var port = process.env.PORT || 5000;
